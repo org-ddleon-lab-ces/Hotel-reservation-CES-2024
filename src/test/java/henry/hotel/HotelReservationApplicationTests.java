d83c3e69fa39c7eb60c181ca03a4afc3238e8600
ddleonces <ddleon@ces.com.uy>
2024-06-18 18:40:57 -0300

    ADD profile test and h2 db

--- src/test/java/henry/hotel/HotelReservationApplicationTests.java
+++ src/test/java/henry/hotel/HotelReservationApplicationTests.java
@@ -2,7 +2,9 @@ package henry.hotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class HotelReservationApplicationTests {
 
