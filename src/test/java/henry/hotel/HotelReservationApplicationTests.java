4d073cae1ee1dbf0f92754fe08e811434c2d1d55
ddleonces <ddleon@ces.com.uy>
2024-06-18 18:19:57 -0300

    Fix testing

--- src/test/java/henry/hotel/HotelReservationApplicationTests.java
+++ src/test/java/henry/hotel/HotelReservationApplicationTests.java
@@ -3,11 +3,11 @@ package henry.hotel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class HotelReservationApplicationTests {
+@SpringBootTest
+class HotelReservationApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
