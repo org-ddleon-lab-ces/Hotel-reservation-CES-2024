cad7d218e90ed968d776473bd85ed298c4ca29b7
ddleonces <ddleon@ces.com.uy>
2024-06-20 08:33:34 -0300

    Add integration tests

---
+++ src/test/java/henry/hotel/HotelReservationControllerIntegrationTests.java
@@ -0,0 +1,45 @@
+package henry.hotel;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import henry.hotel.controller.HotelReservationController;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HotelReservationControllerIntegrationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Autowired
+	private HotelReservationController controller;
+	
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	void greetingShouldReturnDefaultMessage() throws Exception {
+		final String response = this.restTemplate.getForObject(this.getBaseURL(), String.class);
+		final String expected = "<title>Hotel Login</title>";
+
+		assertThat(response).contains(expected);
+	}
+
+	String getBaseURL() {
+		return "http://localhost:" + this.port + "/";
+	}
+	
+}
